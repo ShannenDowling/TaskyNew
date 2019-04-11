4aafe162eeee550d29754e17701542b58493eaba
ShannenDowling <shannen.dowling@students.ittralee.ie>
2019-04-11 18:04:11 +0100

    Update UI Tests

--- TaskyUITest/Tests.cs
+++ TaskyUITest/Tests.cs
@@ -43,8 +43,7 @@ namespace TaskyUITest
         {
             app.Screenshot("First screen - Enter text test.");
             app.Tap(x => x.Marked("Add Task"));
-            app.Tap(x => x.Marked("Name"));
-
+            app.Tap(x => x.Marked("Name:"));
             app.Screenshot("Before calling EnterText");
             app.EnterText("The test worked!");
             app.Screenshot("Text entered");
@@ -57,12 +56,26 @@ namespace TaskyUITest
         {
             app.Screenshot("First screen - Clear text test.");
             app.Tap(x => x.Marked("Add Task"));
-            app.Tap(x => x.Text("Name"));
-
+            app.Tap(x => x.Text("Name:"));
             app.Screenshot("Before calling ClearText");
             app.EnterText("The test worked!");
+            app.Screenshot("Text entered");
             app.ClearText();
             app.Screenshot("Text cleared & replaced");
+            app.Back();
+        }
+
+        [Test]
+        public void SaveTaskExample()
+        {
+            app.Screenshot("First screen - Save task test.");
+            app.Tap(x => x.Marked("Add Task"));
+            app.Tap(x => x.Text("Name:"));
+            app.Screenshot("Before calling SaveText");
+            app.EnterText("New Task Test");
+            app.Screenshot("Text entered");
+            app.Tap(x => x.Marked("Save"));
+            app.Screenshot("Saved");
             app.Back();
         }
     }
